ef2331d7b5dd6adbdf0a17957871772131882945
opacity-black <2690598117@qq.com>
2022-01-20 14:53:31 +0800

    master

---
+++ test.cpp
@@ -0,0 +1,11 @@
+// Ctrl+Alt+N 编译运行！
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Yes, you did it!"<<endl;
+    string s;
+    cout<<"What your name?";
+    cin>>s;
+    cout<<"NB! "<<s;
+    return 0;
+}
